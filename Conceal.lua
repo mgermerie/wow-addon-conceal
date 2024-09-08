036e97ed5c0745a059721e332f87fada59b3526f
Madec Germerie <73115044+mgermerie@users.noreply.github.com>
2024-09-08 13:03:50 +0200

    Update Conceal.lua

--- Conceal.lua
+++ Conceal.lua
@@ -2,42 +2,93 @@ local Conceal = CreateFrame("Frame")
 
 
 function Conceal:ShowMouseOverElements()
-	if MicroMenuContainer:IsMouseOver() then
+
+	if IsAltKeyDown() then return end
+
+	if BagsBar:IsMouseOver() or MicroMenuContainer:IsMouseOver() then
+		
+		BagsBar:SetAlpha(1)
 		MicroMenuContainer:SetAlpha(1)
+	
 	else
+
+		BagsBar:SetAlpha(0)
 		MicroMenuContainer:SetAlpha(0)
+		
 	end 
 end
 
 
 function Conceal:ShowElements()
 
+	BagsBar:SetAlpha(1)
 	MicroMenuContainer:SetAlpha(1)
+	
 end
 
 
 function Conceal:HideElements()
-	
-	if not MicroMenuContainer:IsMouseOver() then
-		MicroMenuContainer:SetAlpha(0);
+
+	if BagsBar:isMouseOver() then
+		
+		MicroMenuContainer:SetAlpha(0)
+		
+	elseif MicroMenuContainer:IsMouseOver() then
+		
+		BagsBar:SetAlpha(0)
+		
+	else
+
+		BagsBar:SetAlpha(0)
+		MicroMenuContainer:SetAlpha(0)
+		
 	end
+	
+end
+
+
+function Conceal:OnInitialize()
+
+	C_Timer.NewTicker(0.25, function()
+		Conceal:ShowMouseOverElements()
+	end)
+	
+end
+
+
+function Conceal:ADDON_LOADED(event, addOnName)
+	
+	if event == "ADDON_LOADED" and (addOnName == "Conceal") then
+		
+		Conceal:OnInitialize()
+		
+	end
+	
 end
 
 
 function Conceal:MODIFIER_STATE_CHANGED(key, inOrOut)
+	
 	if ( IsAltKeyDown() ) then
+		
 		Conceal:ShowElements()
+		
 	else
 		Conceal:HideElements()
+		
 	end
+	
 end
 
 
+Conceal:RegisterEvent("ADDON_LOADED")
 Conceal:RegisterEvent("MODIFIER_STATE_CHANGED")
 
 
 function Conceal:OnEvent(event, ...)
+	
 	self[event](self, event, ...)
+	
 end
 
 
